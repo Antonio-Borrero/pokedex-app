8030608fb7f8c163d7d393765cbe0440bf5b36a0
ANTONIO <antojoseabad@gmail.com>
2025-10-01 19:19:54 +0200

    feat(fetchPokeAPI): add fetchPokemonSpecies and fetchEvolutionChain functions

--- src/api/fetchPokeAPI.ts
+++ src/api/fetchPokeAPI.ts
@@ -1,4 +1,4 @@
-import {PokemonList, Pokemon} from "@/types/pokemon"
+import {PokemonList, Pokemon, PokemonSpecies, PokemonEvolutionChain} from "@/types/pokemon"
 
 export const fetchPokemonSpeciesCount = async (): Promise<number> => {
     const resp = await fetch("https://pokeapi.co/api/v2/pokemon-species");
@@ -22,6 +22,19 @@ export const fetchPokemonByIdOrName = async (idOrName: string | number): Promise
         height: data.height,
         weight: data.weight,
         sprites: data.sprites.other["official-artwork"].front_default,
-        stats: data.stats
+        stats: data.stats,
+        species: data.species.url
     }
+}
+
+export const fetchPokemonSpecies = async (url: string): Promise<PokemonSpecies> => {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return { evolution_chain: data.evolution_chain.url }
+}
+
+export const fetchEvolutionChain = async (url: string): Promise<PokemonEvolutionChain> => {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data.chain
 }
