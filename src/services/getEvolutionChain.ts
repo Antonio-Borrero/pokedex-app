4b4d0a643fb320941e970b19cd190afc17e42d30
ANTONIO <antojoseabad@gmail.com>
2025-10-02 19:26:26 +0200

    feat(services): add getEvolutionChain service to fetch and normalize evolution chain

---
+++ src/services/getEvolutionChain.ts
@@ -0,0 +1,15 @@
+import {fetchEvolutionChain, fetchPokemonSpecies} from "@/api/fetchPokeAPI";
+import {PokemonEvolutionChain} from "@/types/pokemon";
+import {getPokemonIdFromUrl} from "@/utils/getPokemonIdFromUrl";
+
+const extractIdsFromChain = (chain: PokemonEvolutionChain): number[] => {
+    const currentPokemonId = getPokemonIdFromUrl(chain.species.url);
+    const evolutionsId = chain.evolves_to.flatMap(Evolution => extractIdsFromChain(Evolution));
+    return [currentPokemonId, ...evolutionsId]
+}
+
+export const getEvolutionChain = async (url: string) => {
+    const pokemonSpecies = await fetchPokemonSpecies(url);
+    const evolutionChain = await fetchEvolutionChain(pokemonSpecies.evolution_chain);
+    return extractIdsFromChain(evolutionChain)
+}
